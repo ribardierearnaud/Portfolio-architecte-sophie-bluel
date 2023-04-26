935d3f49c7d64eb58c6e106064615203dea872f0
nono2chevres <arnaud.ribardiere@gmail.com>
2023-04-26 21:25:34 +0200

    Adding dynamic filters

--- FrontEnd/assets/assets.js
+++ FrontEnd/assets/assets.js
@@ -1,13 +1,46 @@
-// Récupération des travaux depuis l'API en local
+// Récupération des travaux depuis l'API
 const reponse = await fetch(`http://localhost:5678/api/works/`);
 const works = await reponse.json();
 
-generateWorks(works);
+// Génération de la page sans filtre
+generateWorks("Tous");
 
-function generateWorks(works){
-    for (let i = 0; i < works.length; i++) {
+export default function generateWorks(category) {
+    // on supprime l'ancien contenu
+    document.querySelector(".gallery").innerHTML = "";
+
+    if (category === "Tous") {
+        for (let i = 0; i < works.length; i++) {
+
+            const figure = works[i];
+            // Récupération de l'élément du DOM qui peuplera la "gallery"
+            const sectionGallery = document.querySelector(".gallery");
+            // Création d’une balise dédiée à un projet d'architecture
+            const figureElement = document.createElement("figure");
+            // Création des balises 
+            const imageElement = document.createElement("img");
+            imageElement.src = figure.imageUrl;
+            const figcaptionElement = document.createElement("figcaption");
+            figcaptionElement.innerText = figure.title;
+    
+            
+            // On rattache la balise figure à la section Gallery
+            sectionGallery.appendChild(figureElement);
+            figureElement.appendChild(imageElement);
+            figureElement.appendChild(figcaptionElement);
+        }
+    
+    }
+    
+    else for (let i = 0; i < works.length; i++) {
 
         const figure = works[i];
+        const categoryWork = works[i].category.id;
+
+        console.log(categoryWork);
+        console.log(category);
+
+        if (categoryWork ===  category) {
         // Récupération de l'élément du DOM qui peuplera la "gallery"
         const sectionGallery = document.querySelector(".gallery");
         // Création d’une balise dédiée à un projet d'architecture
@@ -23,5 +56,7 @@ function generateWorks(works){
         sectionGallery.appendChild(figureElement);
         figureElement.appendChild(imageElement);
         figureElement.appendChild(figcaptionElement);
+        }
     }
 }
+
---
+++ FrontEnd/assets/categories.js
@@ -0,0 +1,65 @@
+import generateWorks from "./assets.js";
+
+// Récupération des travaux depuis l'API
+const reponse = await fetch(`http://localhost:5678/api/categories/`);
+const categories = await reponse.json();
+
+// Génération de la page avec le filtre "Tous"
+generateFiltersPerCategories(categories);
+
+async function generateFiltersPerCategories(categories) {
+    // Création du filtre "Tous"
+    const sectionGalleryFilter = document.querySelector(".gallery-filters");
+    const allFilterElement = document.createElement("button");
+    // Ajout de la classe gallery-filter
+    allFilterElement.classList.add("gallery-filter"); 
+    // Ajout de la classe selectionné au chargement de la page
+    allFilterElement.classList.add("selected"); 
+    // Ajout du libellé Tous
+    allFilterElement.innerText = "Tous";
+
+    // On ajoute l'event listener pour permettre la sélection des données et le changement d'affichage du bouton
+    allFilterElement.addEventListener("click", function() {
+        // Désélection de tous les autres filtres
+        deselectAllFilters();
+        // Sélection du filtre "All"
+        allFilterElement.classList.add("selected");
+        // Appel à la fonction pour filtrer les travaux avec la catégorie "All"
+        generateWorks(allFilterElement.innerText);
+    });
+
+    // On rattache l'élément filterElement à la section galleryFilter
+    sectionGalleryFilter.appendChild(allFilterElement);
+
+    // Itération à travers les catégories récupérées depuis l'API
+    for (let i = 0; i < categories.length; i++) {
+        const category = categories[i];
+        // Création d'un élément div dédié à un filtre par nature de projet
+        const filterElement = document.createElement("button");
+        // Ajout de la classe gallery-filter
+        filterElement.classList.add("gallery-filter"); 
+        // Création des balises contenant le nom de la catégorie
+        filterElement.innerText = category.name;
+        // On rattache l'élément filterElement à la section galleryFilter
+        sectionGalleryFilter.appendChild(filterElement);
+
+        // On ajoute l'event listener pour permettre la sélection des données et le changement d'affichage du bouton
+        filterElement.addEventListener("click", function() {
+            console.log(filterElement);
+            // Désélection de tous les autres filtres
+            deselectAllFilters();
+            // Sélection du filtre cliqué
+            filterElement.classList.add("selected");
+            // Appel à la fonction pour filtrer les travaux avec la catégorie correspondante
+            generateWorks(category.id);
+        });
+    }
+
+    // Fonction pour désélectionner tous les filtres
+    function deselectAllFilters() {
+        const filterElements = document.querySelectorAll(".gallery-filter");
+        filterElements.forEach(filterElement => {
+            filterElement.classList.remove("selected");
+        });
+    }
+}
